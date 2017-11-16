ec8789d6e1d775d459fccd8e4c48d4bd558eaa85
Roake <jojo_kitvivathkul@windowslive.com>
2017-11-17 00:28:04 +0700

    update

--- WebContent/js/Menu.js
+++ WebContent/js/Menu.js
@@ -13,22 +13,23 @@ Menu.prototype.preload = function() {
 	this.load.pack("start", "assets/assets-pack.json");
 };
 
-Menu.prototype.create = function() {
-	this.stage.backgroundColor = "#bb0406";
-	var sprite = this.add.sprite(this.world.100, this.world.100,
-			"Title");
-	var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
-	"Start-Icon");
-	var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
-	"Setting-Icon");
-	var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
-	"Dev-Icon");
-	var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
-	"Exit");
-	sprite.anchor.set(0.5, 0.5);
-	this.input.onDown.add(this.startGame, this);
-};
 
+
+Menu.prototype.create = function() {
+		this.stage.backgroundColor = "#bb0406";
+		var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
+				"Title");
+		sprite.anchor.set(0.5, 0.5);
+		this.input.onDown.add(this.startGame, this);
+		this.exit= this.exit (130,100);
+	};
+	
+	Menu.prototype.exit = function(x,y) {
+		R= this.add.sprite(x,y,"Exit");
+		R.anchor.set(0.5, 0.5);
+		return R;
+	}
+	
 Menu.prototype.startGame = function() {
 	this.game.state.start("Level");
 };
