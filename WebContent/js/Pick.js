eb770d0a4a40db2a66ebf212b6c97d9863ad9cc1
Roake <jojo_kitvivathkul@windowslive.com>
2017-11-21 01:47:44 +0700

    Pick

--- WebContent/js/Pick.js
+++ WebContent/js/Pick.js
@@ -12,14 +12,33 @@ Pick.prototype = proto;
  *
  */
 
-Dev.prototype.create = function() {
+Pick.prototype.create = function() {
 	var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
 	"PickPage");
 sprite.anchor.set(0.5, 0.5);
-
-this.input.onDown.add(this.startGame, this);
+this.orange1= this.addWinston (130,100);
 }
 
-Dev.prototype.startGame = function() {
+Level.prototype.addMinister = function(x, y) {
+	var a = this.add.sprite(x, y, "Winston");
+	a.animations.add("Idle", gframes("Winston-Idle", 1), 1, true,false);
+	a.anchor.set(0.5, 1);
+	a.scale.set (0.5);
+	a.smoothed = false;
+	this.game.physics.arcade.enable(a);
+	a.play("Idle");
+	a.body.collideWorldBounds = true;
+	return a;
+};
+
+function gframes(key, n) {
+	f = [];
+	for (var i = 0; i <= n; i++) {
+		f.push(key + "_" + "00" + i);
+	}
+	return f;
+}
+
+Pick.prototype.startGame = function() {
 	this.game.state.start("Level");
 };
