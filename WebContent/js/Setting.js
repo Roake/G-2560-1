c17326a7e456f0acd7bb7e896550dea50201350a
ShadowX <ShadowX@DESKTOP-HTLG27G>
2017-11-21 16:12:18 +0700

    Update Setting

--- WebContent/js/Setting.js
+++ WebContent/js/Setting.js
@@ -16,7 +16,7 @@ function Setting () {
 }
 
 Setting.prototype.create = function (){
-	var text = this.add.text(this.world.centerX-100, this.world.centerY, "Not Available", {fill: 'white'});
+	var text = this.add.text(this.world.centerX-100, this.world.centerY, "Not Available in Alpha Version", {fill: 'white'});
 	text.scale.set(1);
 	this.input.onDown.add(this.startGame,this);
 	
