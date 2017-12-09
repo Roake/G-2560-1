df08da84d551d1d41c50c5e0d4561e95bccf1e37
Roake <jojo_kitvivathkul@windowslive.com>
2017-12-09 10:22:53 +0700

    dd

--- WebContent/js/Cutscene.js
+++ WebContent/js/Cutscene.js
@@ -12,14 +12,14 @@ Cutscene.prototype = proto;
 
 Cutscene.prototype.create = function() {
 	var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
-	"DevPage");
+	"PG1");
 sprite.anchor.set(0.5, 0.5);
 this.input.onDown.add(this.startGame, this);
 var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'white'});
 text.scale.set(1);
 }
 
-Dev.prototype.startGame = function() {
+Cutscene.prototype.startGame = function() {
 	this.game.state.start("Pick");
 };
 
