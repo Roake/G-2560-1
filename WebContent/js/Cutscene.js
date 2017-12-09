8882516e0e45f0ad22ac4ed4e42a4120014e11e8
Roake <jojo_kitvivathkul@windowslive.com>
2017-12-09 10:41:25 +0700

    CC

--- WebContent/js/Cutscene.js
+++ WebContent/js/Cutscene.js
@@ -11,13 +11,48 @@ Cutscene.prototype = proto;
 
 
 Cutscene.prototype.create = function() {
-	var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
+	var p1 = this.add.sprite(this.world.centerX, this.world.centerY,
 	"PG1");
-sprite.anchor.set(0.5, 0.5);
-this.input.onDown.add(this.startGame, this);
+p1.anchor.set(0.5, 0.5);
+this.input.onDown.add(this.PG2, this);
 var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'white'});
 text.scale.set(1);
 }
+
+Cutscene.prototype.PG2 = function() {
+	var p2 = this.add.sprite(this.world.centerX, this.world.centerY,
+	"PG2");
+p2.anchor.set(0.5, 0.5);
+this.input.onDown.add(this.PG3, this);
+var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'white'});
+text.scale.set(1);
+};
+
+Cutscene.prototype.PG3 = function() {
+	var p3 = this.add.sprite(this.world.centerX, this.world.centerY,
+	"PG3");
+p3.anchor.set(0.5, 0.5);
+this.input.onDown.add(this.PG4, this);
+var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'white'});
+text.scale.set(1);
+};
+Cutscene.prototype.PG4 = function() {
+	var p4 = this.add.sprite(this.world.centerX, this.world.centerY,
+	"PG4");
+p4.anchor.set(0.5, 0.5);
+this.input.onDown.add(this.PG5, this);
+var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'white'});
+text.scale.set(1);
+};
+Cutscene.prototype.PG5 = function() {
+	var p5 = this.add.sprite(this.world.centerX, this.world.centerY,
+	"PG5");
+p5.anchor.set(0.5, 0.5);
+this.input.onDown.add(this.startGame, this);
+var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'white'});
+text.scale.set(1);
+};
+
 
 Cutscene.prototype.startGame = function() {
 	this.game.state.start("Pick");
