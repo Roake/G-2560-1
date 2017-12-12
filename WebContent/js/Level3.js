b26a925759e9ebdf9d8c94abac5dd0e17effffe3
ShadowX <ShadowX@DESKTOP-LJFC8U1>
2017-12-12 21:50:44 +0700

    call state

---
+++ WebContent/js/Level3.js
@@ -0,0 +1,10 @@
+/**
+ *Level3 state.
+ */
+function Level3() {
+	Phaser.State.call(this);
+}
+
+/** @type Phaser.State */
+var proto = Object.create(Phaser.State);
+Level3.prototype = proto;
--- WebContent/js/Level4.js
+++ WebContent/js/Level4.js
@@ -1,7 +1,10 @@
 /**
- *
+ *Level4 state.
  */
-function Level4 () {
-	
+function Level4() {
+	Phaser.State.call(this);
 }
 
+/** @type Phaser.State */
+var proto = Object.create(Phaser.State);
+Level4.prototype = proto;
