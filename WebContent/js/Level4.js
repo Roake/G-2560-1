d72585c86df070312222e7208ea522a809c3bb6d
Roake <jojo_kitvivathkul@windowslive.com>
2017-12-14 12:02:00 +0700

    too Gundam

--- WebContent/js/Level4.js
+++ WebContent/js/Level4.js
@@ -16,6 +16,8 @@ Level4.prototype.create = function() {
 	background.fixedToCamera = true;
 	this.lift = this.add.audio("lift");
 	this.lift.play();
+	this.music = this.add.audio("0039");
+	this.music.loopFull()
 	this.map = this.game.add.tilemap("c1-4");
 	this.map.addTilesetImage('industrial.v1');
 	
