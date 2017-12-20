321d6bcd56a7de3061ac713ab81ddcaeae378798
Roake <jojo_kitvivathkul@windowslive.com>
2017-12-21 01:08:15 +0700

    Fix cut

--- WebContent/js/Mid.js
+++ WebContent/js/Mid.js
@@ -1,16 +1,16 @@
 /**
- *MidCutscene state.
+ *Mid state.
  */
-function MidCutscene() {
+function Mid() {
 	Phaser.State.call(this);
 }
 
 /** @type Phaser.State */
 var proto = Object.create(Phaser.State);
-MidCutscene.prototype = proto;
+Mid.prototype = proto;
 
 
-MidCutscene.prototype.create = function() {
+Mid.prototype.create = function() {
 	var a = this.add.sound("ost",0.5,true,true);
 	a.play();
 	
@@ -21,7 +21,7 @@ var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'whit
 text.scale.set(1);
 }
 
-MidCutscene.prototype.PG2 = function() {
+Mid.prototype.PG2 = function() {
 	var p2 = this.add.button(this.world.centerX, this.world.centerY,
 	"MID2",this.PG3, this);
 p2.anchor.set(0.5, 0.5);
@@ -29,7 +29,7 @@ var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'whit
 text.scale.set(1);
 };
 
-MidCutscene.prototype.PG3 = function() {
+Mid.prototype.PG3 = function() {
 	var p3 = this.add.button(this.world.centerX, this.world.centerY,
 	"MID3",this.PG4, this);
 p3.anchor.set(0.5, 0.5);
@@ -37,7 +37,7 @@ var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'whit
 text.scale.set(1);
 };
 
-MidCutscene.prototype.PG4 = function() {
+Mid.prototype.PG4 = function() {
 	var p4 = this.add.button(this.world.centerX, this.world.centerY,
 	"MID4",this.startGame, this);
 p4.anchor.set(0.5, 0.5);
@@ -46,7 +46,7 @@ text.scale.set(1);
 };
 
 
-MidCutscene.prototype.startGame = function() {
+Mid.prototype.startGame = function() {
 	this.cache.removeSound("ost");
 	this.game.state.start("Level5");
 };
