217ed15030cf33ffe3a576368f35f3390dd52ce7
Roake <jojo_kitvivathkul@windowslive.com>
2017-12-21 13:04:26 +0700

    ddd

--- WebContent/js/End.js
+++ WebContent/js/End.js
@@ -10,3 +10,14 @@ var proto = Object.create(Phaser.State);
 End.prototype = proto;
 
 
+End.prototype.create = function() {
+	var p1 = this.add.button(this.world.centerX, this.world.centerY,
+			"END",this.PG2, this);
+		p1.anchor.set(0.5, 0.5);
+		var text = this.add.text(10, this.world.height-30, "Alpha Version", {fill: 'white'});
+		text.scale.set(1);
+		}
+End.prototype.PG2 = function() {
+	this.game.state.start("Menu");
+};
+
